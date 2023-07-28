fcf9d21cdcff22533e657ea96fad52ffaa35d146
dotjalil <mgkmohammad@gmail.com>
2023-07-28 21:17:56 +0300

    Task 1: Error Handling

--- JavaScript/Lab 6/Error Handling/error-handling.js
+++ JavaScript/Lab 6/Error Handling/error-handling.js
@@ -1,1 +1,38 @@
-console.log("hello from Jimmiy's setup");
+function acceptOnly2Parameters(...p) {
+  if (p.length !== 2) {
+    throw Error("Only 2 arguments are allowed");
+  }
+}
+
+// acceptOnly2Parameters(1, 2, 3);
+
+function addNNumbers(...n) {
+  if (n.length < 1) {
+    throw Error("Please pass one or more parameters");
+  }
+  var result = 0;
+  n.forEach((num) => {
+    if (typeof num !== "number") {
+      throw Error("Only numbers are accepted");
+    } else {
+      result += num;
+    }
+  });
+  return result;
+}
+
+// addNNumbers(1, 2, 3, 4);
+
+function reverseItems(...items) {
+  return [...items].reverse();
+}
+
+// reverseItemsBySpread(1, 2, 3);
+
+function reverseItemsByReducer(...items) {
+  return items.reduceRight((accumulator, value) => {
+    return [...accumulator, value];
+  }, []);
+}
+
+// reverseItemsByReducer(1, 2, 3);
